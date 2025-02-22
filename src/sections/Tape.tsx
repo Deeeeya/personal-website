82240a8dedf35de22836946008a8de5ed0d55220
Deeeeya <159756541+Deeeeya@users.noreply.github.com>
2025-02-22 13:44:51 -0500

    finished projects section + started tape section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,27 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div>
+      {words.map((word) => (
+        <div key={word}>
+          <span>{word}</span>
+          <StarIcon />
+        </div>
+      ))}
+    </div>
+  );
 };
