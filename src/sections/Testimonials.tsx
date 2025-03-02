42d84da1e1fae72c7f8897714234c278e28cb91b
Deeeeya <tankonated@gmail.com>
2025-03-02 01:24:58 -0500

    update avatar

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,5 +1,5 @@
-import memojiAvatar1 from "@/assets/images/memoji-avatar-1.png";
-import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
+import memojiAvatar1 from "@/assets/images/mustafa-avatar.png";
+import memojiAvatar2 from "@/assets/images/nader-avatar.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
